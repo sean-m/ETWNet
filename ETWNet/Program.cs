98ce90743e11dae88c0ff02200f9c234df7149dd
Sean McArdle <sean@mcardletech.com>
2025-04-08 12:08:47 -0700

    Well that's working now. Session name is load bearing.

--- ETWNet/Program.cs
+++ ETWNet/Program.cs
@@ -13,21 +13,17 @@ namespace McETWNet {
         static void Main(string[] args)
         {
             // Local process cache to speed lookup later
-            var processes = Process.GetProcesses().Select(p => new pinfo
-            {
-                Name = p.ProcessName,
-                Id = p.Id
-            }).ToDictionary(p => p.Id);
+            var processes = Process.GetProcesses()
+                .ToDictionary(p => p.Id, p => p.ProcessName);
 
             string TryGetProcessName(TraceEvent evt)
             {
                 if (!string.IsNullOrEmpty(evt.ProcessName))
                     return evt.ProcessName;
-                return processes.TryGetValue(evt.ProcessID, out var info) ? info.Name : string.Empty;
+                return processes.TryGetValue(evt.ProcessID, out var name) ? name : string.Empty;
             }
 
-
-            using (var session = new TraceEventSession(Environment.OSVersion.Version.Build >= 9200 ? "McETWNetSession" : KernelTraceEventParser.KernelSessionName))
+            using (var session = new TraceEventSession(Environment.OSVersion.Version.Build >= 9200 ? "MyKernelSession" : KernelTraceEventParser.KernelSessionName))
             {
                 session.EnableKernelProvider(KernelTraceEventParser.Keywords.NetworkTCPIP);
                 var parser = session.Source.Kernel;
@@ -38,81 +34,80 @@ namespace McETWNet {
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
                     var name = TryGetProcessName(e);
-                    Console.WriteLine($"{e.TimeStamp}.{e.TimeStamp.Millisecond:D3}: TCP\tConnect\t{e.connid}\t{e.saddr}:{e.sport} -> {e.daddr}:{e.dport}\t{e.ProcessID}\t{name}");
+                    Console.WriteLine($"{e.TimeStamp.ToString("O")}: TCP\tConnect\t{e.connid}\t{e.saddr}:{e.sport} -> {e.daddr}:{e.dport}\t{e.ProcessID}\t{name}");
                 };
 
                 parser.TcpIpConnectIPV6 += e =>
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
                     var name = TryGetProcessName(e);
-                    Console.WriteLine($"{e.TimeStamp}.{e.TimeStamp.Millisecond:D3}: TCPv6\tConnect\t{e.connid}\t{e.saddr}:{e.sport} -> {e.daddr}:{e.dport}\t{e.ProcessID}\t{name}");
+                    Console.WriteLine($"{e.TimeStamp.ToString("O")}: TCPv6\tConnect\t{e.connid}\t{e.saddr}:{e.sport} -> {e.daddr}:{e.dport}\t{e.ProcessID}\t{name}");
                 };
 
                 parser.TcpIpDisconnect += e =>
                 {
                     Console.ForegroundColor = ConsoleColor.DarkYellow;
                     var name = TryGetProcessName(e);
-                    Console.WriteLine($"{e.TimeStamp}.{e.TimeStamp.Millisecond:D3}: TCP\tDisconnect\t{e.connid}\t{e.saddr}:{e.sport} -> {e.daddr}:{e.dport}\t{e.ProcessID}\t{name}");
+                    Console.WriteLine($"{e.TimeStamp.ToString("O")}: TCP\tDisconn\t{e.connid}\t{e.saddr}:{e.sport} -> {e.daddr}:{e.dport}\t{e.ProcessID}\t{name}");
                 };
 
                 parser.TcpIpDisconnectIPV6 += e =>
                 {
                     Console.ForegroundColor = ConsoleColor.DarkYellow;
                     var name = TryGetProcessName(e);
-                    Console.WriteLine($"{e.TimeStamp}.{e.TimeStamp.Millisecond:D3}: TCPv6\tDisconnect\t{e.connid}\t{e.saddr}:{e.sport} -> {e.daddr}:{e.dport}\t{e.ProcessID}\t{name}");
+                    Console.WriteLine($"{e.TimeStamp.ToString("O")}: TCPv6\tDisconn\t{e.connid}\t{e.saddr}:{e.sport} -> {e.daddr}:{e.dport}\t{e.ProcessID}\t{name}");
                 };
 
                 parser.TcpIpFail += e =>
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     var name = TryGetProcessName(e);
-                    Console.WriteLine($"{e.TimeStamp}.{e.TimeStamp.Millisecond:D3}: TCP\tFail\t\txxxx:xx - xxxx:xx\t{e.ProcessID}\t{name}\t{e.FailureCode}\t{e.FormattedMessage}");
+                    Console.WriteLine($"{e.TimeStamp.ToString("O")}: TCP\tFail\t\txxxx:xx - xxxx:xx\t{e.ProcessID}\t{name}\t{e.FailureCode}\t{e.FormattedMessage}");
                 };
 
                 parser.TcpIpRetransmit += e =>
                 {
                     Console.ForegroundColor = ConsoleColor.Magenta;
                     var name = TryGetProcessName(e);
-                    Console.WriteLine($"{e.TimeStamp}.{e.TimeStamp.Millisecond:D3}: TCP\tRetransmit\t{e.connid}\t{e.saddr}:{e.sport} -> {e.daddr}:{e.dport}\t{e.ProcessID}\t{name}");
+                    Console.WriteLine($"{e.TimeStamp.ToString("O")}: TCP\tRetr\t{e.connid}\t{e.saddr}:{e.sport} -> {e.daddr}:{e.dport}\t{e.ProcessID}\t{name}");
                 };
 
                 parser.TcpIpRetransmitIPV6 += e =>
                 {
                     Console.ForegroundColor = ConsoleColor.Magenta;
                     var name = TryGetProcessName(e);
-                    Console.WriteLine($"{e.TimeStamp}.{e.TimeStamp.Millisecond:D3}: TCPv6\tRetransmit\t{e.connid}\t{e.saddr}:{e.sport} -> {e.daddr}:{e.dport}\t{e.ProcessID}\t{name}");
+                    Console.WriteLine($"{e.TimeStamp.ToString("O")}: TCPv6\tRetr\t{e.connid}\t{e.saddr}:{e.sport} -> {e.daddr}:{e.dport}\t{e.ProcessID}\t{name}");
                 };
 
                 parser.UdpIpSend += e =>
                 {
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     var name = TryGetProcessName(e);
-                    Console.WriteLine($"{e.TimeStamp}.{e.TimeStamp.Millisecond:D3}: UDP\tSend\t\t{e.saddr}:{e.sport} -> {e.daddr}:{e.dport}\t{e.ProcessID}\t{name}");
+                    Console.WriteLine($"{e.TimeStamp.ToString("O")}: UDP\tSend\t\t{e.saddr}:{e.sport} -> {e.daddr}:{e.dport}\t{e.ProcessID}\t{name}");
                 };
 
                 parser.UdpIpRecv += e =>
                 {
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     var name = TryGetProcessName(e);
-                    Console.WriteLine($"{e.TimeStamp}.{e.TimeStamp.Millisecond:D3}: UDP\tRecv\t\t{e.daddr}:{e.dport} <- {e.saddr}:{e.sport}\t{e.ProcessID}\t{name}");
+                    Console.WriteLine($"{e.TimeStamp.ToString("O")}: UDP\tRecv\t\t{e.daddr}:{e.dport} <- {e.saddr}:{e.sport}\t{e.ProcessID}\t{name}");
                 };
 
                 parser.UdpIpSendIPV6 += e =>
                 {
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     var name = TryGetProcessName(e);
-                    Console.WriteLine($"{e.TimeStamp}.{e.TimeStamp.Millisecond:D3}: UDPv6\tSend\t{e.connid}\t{e.saddr}:{e.sport} -> {e.daddr}:{e.dport}\t{e.ProcessID}\t{name}");
+                    Console.WriteLine($"{e.TimeStamp.ToString("O")}: UDPv6\tSend\t{e.connid}\t{e.saddr}:{e.sport} -> {e.daddr}:{e.dport}\t{e.ProcessID}\t{name}");
                 };
 
                 parser.UdpIpRecvIPV6 += e =>
                 {
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     var name = TryGetProcessName(e);
-                    Console.WriteLine($"{e.TimeStamp}.{e.TimeStamp.Millisecond:D3}: UDPv6\tRecv\t\t{e.daddr}:{e.dport} <- {e.saddr}:{e.sport}\t{e.ProcessID}\t{name}");
+                    Console.WriteLine($"{e.TimeStamp.ToString("O")}: UDPv6\tRecv\t\t{e.daddr}:{e.dport} <- {e.saddr}:{e.sport}\t{e.ProcessID}\t{name}");
                 };
 
-                Task.Run(() => session.Source.Process());
-                while (true) { Thread.Sleep(10); }
+                Task.Run(() => session.Source.Process()).Wait();
             }
         }
     }
